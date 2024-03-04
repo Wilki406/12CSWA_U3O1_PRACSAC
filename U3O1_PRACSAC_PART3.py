531fdf19e2b0b23f84150d4af416f2b6a74ce9b1
Benjamin <134766715+Wilki406@users.noreply.github.com>
2024-03-04 14:26:49 +1100

    Update U3O1_PRACSAC_PART3.py

--- U3O1_PRACSAC_PART3.py
+++ U3O1_PRACSAC_PART3.py
@@ -5,6 +5,7 @@
 # imports
 import csv
 import PySimpleGUI as sg
+import itertools
 
 # Set the theme
 sg.theme("BrightColors")
@@ -27,7 +28,7 @@ alldata = []
 
 
 # Read data from CSV file
-with open('data.csv', 'r') as file:
+with open('Data/newdata.csv') as file:
     # setting the reader to be a csv dictionary reader
     reader = csv.DictReader(file)
     # going through each column
@@ -46,9 +47,7 @@ with open('data.csv', 'r') as file:
 for textbook, subject, seller, purchase, purchaser, sale, rating in zip(textbooks, subjects, seller, purchasePrices, purchaser, salePrices, rating):
     alldata.append([textbook, subject, seller, purchase, purchaser, sale, rating])
 
-
-
-headers = ['textbook', 'subject', 'seller', 'purchased', 'purchaser', 'sale price', 'rating']
+headers = ['Textbook', 'Subject', 'Seller', 'Purchase price', 'Purchaser', 'Sale price', 'Rating']
 # Create the table separate from the layout
 tbl1 = sg.Table(values=alldata, headings=headers,
                 auto_size_columns=True,
@@ -66,10 +65,11 @@ layout = [[tbl1],
            [sg.Text("New Rating (1-5): "), sg.Input(key="txt_rating")],
            [sg.Button("Rate & Save", key='save')]]]
 
+
+
 # Create window
 window = sg.Window("Prac Sac", layout, icon='', size=(1200, 400), resizable=True)
 print(tabledata)
-
 # while loop to enable button functionality.
 while True:
     event, values = window.read()
@@ -87,8 +87,7 @@ while True:
         textbooktxt = str(values["txt_search1"])
         purchasertxt = str(values["txt_search2"])
         search_parts = [str(values["txt_search1"]), str(values["txt_search2"])]
-
-        with open('data.csv', 'r') as file:
+        with open('Data/newdata.csv', 'r') as file:
             for row in file:
                 if all([x in row for x in search_parts]):
                     searchdata.append(row)
@@ -96,7 +95,33 @@ while True:
         for value in searchdata:
             tabledata.append(value.strip().split(','))
 
-        window["table"].update(values=tabledata)  # No need to skip headers in the update
+        window["table"].update(values=tabledata)
+        print()
+
+    if event == "save":
+        allowedvalues = ['1','2','3','4','5']
+        selected_textbook = str(values["txt_search1"])
+        selected_purchaser = str(values["txt_search2"])
+        enteredRating = str(values["txt_rating"])
+        if enteredRating not in allowedvalues:
+            sg.popup_error("Please rate 1-5")
+            continue
+
+        for i, row in enumerate(alldata):
+            print(row[0])
+            if selected_textbook in row[0] and selected_purchaser in row[4]:
+                alldata[i][-1] = enteredRating
+
+        with open('Data/newdata.csv', 'w', newline='') as file:
+            writer = csv.writer(file)
+            writer.writerow(headers)
+            writer.writerows(alldata)
+
+        # for alldata in alldata:
+
+        print(alldata)
+        window["table"].update(values=alldata)
+
 
 # Close the window
 window.close()
